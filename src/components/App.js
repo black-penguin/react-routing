70e4b47358a37f42d66bf3fd02e1cbe294dafa92
Austin Wheelwright <pyrotechnician25@gmail.com>
2017-06-19 15:09:57 -0600

    completed steps 1-7

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import Nav from './Nav/Nav';
+import router from '../router';
 import Cart from './Cart/Cart';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
       <Nav />
       <div id="App__mainContainer">
         <div id="App__routerContainer">
+          { router }
           <Cart />
         </div>
       </div>
--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -1,19 +1,24 @@
 import React from "react";
 import { addToCart } from "../../ducks/reducer";
 import { connect } from "react-redux";
+import {Link} from "react-router-dom";
 
 import './Details.css';
 
-function Details( { swag, history, addToCart } ) {
+function Details( { swag, history, addToCart } )
+{
 
-  function addAndRedirect() {
+  function addAndRedirect()
+  {
     addToCart( swag.id );
     history.push('/');
-  } 
+  }
 
   return (
     <div id="Details__container">
-      <p> { '<' } Back to store </p>
+      <Link id="Details__StoreLink" to="/">
+        <p> { '<' } Back to store </p>
+      </Link>
       <img id="Details__img" src={ swag.img } alt="Product" width="400px" />
       <p id="Details__title"> { swag.title } </p>
       <p id="Details__price"> ${ swag.price } </p>
